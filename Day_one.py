246bb06bed4d37044d77f7b0f1606778316d8528
unknown <dihfahsihm@gmail.com>
2022-07-26 00:39:17 +0300

    anothe way of counting objects

--- Day_one.py
+++ Day_one.py
@@ -1,7 +1,15 @@
 #Pythonic way of counting objects
-word = "python"
-counter = ()
+word = "DIHFAHSIH"
+counter = {}
 for letter in word:
     if letter not in counter:
         counter[letter]=0
     counter[letter] +=1
+print(counter)
+
+#another way of counting objects using get()
+word = "DIHFAHSIH"
+counter = {}
+for letter in word:
+    counter[letter]=counter.get(letter, 0) +1 #we are getting the curent count of a given letter or 0 by default if the letter is missing, then we increment the count by 1 and store it under the corresponding letter in the dectionary
+print(counter)
