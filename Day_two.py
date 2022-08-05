a880d87559337b311edfe4c2f979c35bfc1fd3d5
unknown <dihfahsihm@gmail.com>
2022-08-05 23:17:10 +0300

    save to the file

--- Day_two.py
+++ Day_two.py
@@ -3,3 +3,9 @@
 file=open('example1.txt', 'r')
 f=file.read()
 print(f)
+
+#creating a file using write()
+file=open('example2.txt','w')
+file.write("This the first line")
+file.write("This is the second line")
+file.close()
