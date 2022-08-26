a6ba0de465f5867c57cbb3749c16a7029ca24328
Am Pythonista <dihfahsihm@gmail.com>
2022-08-26 22:06:02 +0300

    using lambda with a map function

--- Day_Three.py
+++ Day_Three.py
@@ -4,11 +4,13 @@ multiply=lambda x, y: x * y
 print(multiply(2,3))
 
 '''python Global collections'''
-
 number=[1,2,3]
+# def double(a):
+#   return a * 2 
+# result = map(double, number)
+# print(list(result))
 
-def double(a):
-  return a * 2 
-
-result = map(double, number)
-print(list(result))
+# we can achieve the same using the lambda function
+double = lambda a: a*2
+res=map(double,number)
+print(list(res))
