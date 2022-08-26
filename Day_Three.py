1fb3fe8f91045ee27d1df91da8051c944de435b5
Am Pythonista <dihfahsihm@gmail.com>
2022-08-26 22:11:34 +0300

    using lambda with a filter function

--- Day_Three.py
+++ Day_Three.py
@@ -4,13 +4,15 @@ multiply=lambda x, y: x * y
 print(multiply(2,3))
 
 '''python Global collections'''
-number=[1,2,3]
+number=[1,2,3, 4, 5,6 ,8, 9, 10]
 # def double(a):
 #   return a * 2 
 # result = map(double, number)
 # print(list(result))
 
 # we can achieve the same using the lambda function
-double = lambda a: a*2
-res=map(double,number)
+res=map(lambda a: a*2,number)
+print(list(res))
+
+res=filter(lambda a: a % 2 ==0 ,number)
 print(list(res))
