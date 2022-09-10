8a1edf4cf7ba001159fb5b668b4c45550b282345
Am Pythonista <dihfahsihm@gmail.com>
2022-09-10 09:02:01 +0300

    use mode to get only one

--- Day_Six.py
+++ Day_Six.py
@@ -1,5 +1,9 @@
 from collections import Counter
+from statistics import mode
 
-numbers=[1,5,3,4,6,7,8,3,5,6,9,4,3,5,2]
+numbers=[1,5,3,4,6,7,8,3,5,6,9,4,4,5,2]
 c=Counter(numbers)
 print(c.most_common(3))
+
+#mode returns the first most commonest in the list
+#print(mode(numbers))
